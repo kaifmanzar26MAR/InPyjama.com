819abe04299f6f45ac713fdd2a82728e097a9ab8
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-04-07 16:59:03 +0530

    responsive done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        'sm-md':{'max':'450px'}
+      }
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
